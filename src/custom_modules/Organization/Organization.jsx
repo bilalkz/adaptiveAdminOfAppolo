678b81822cf33375ede9a0d227c210d9ca816234
AnasYAsin <anas@kvsocial.com>
2019-01-23 17:37:39 +0500

    organization component

--- src/custom_modules/Organization/Organization.jsx
+++ src/custom_modules/Organization/Organization.jsx
@@ -37,9 +37,15 @@ import { reduxInput, reduxTextarea } from '../../components/reduxInput/reduxInpu
 import { reduxDatepicker } from '../../components/reduxDatePicker/reduxDatepicker';
 
 const initialState = {
+    orgName: '',
+    orgAddress: '',
+    orgType: '',
+    orgTimeZone: '',
+    orgPlan: '',
     modalVisible: false,
     modalHeader: '',
     activeTab: '1',
+    row: [],
     OrganizationList: [{
         id: '1',
         organization_name: 'Spacesoft',
@@ -51,7 +57,7 @@ const initialState = {
     },
     {
         id: '2',
-        organization_name: 'Spacesoft',
+        organization_name: 'STN Technologies',
         organization_address: '100/4, road-6, Hubstaff',
         organization_type: 'Construction',
         organization_timezone: 'Dhaka, Bangladesh UTC +6',
@@ -83,6 +89,12 @@ class Organization extends React.Component {
         }
     }
 
+    handleChange = (e) => {
+        console.log(e.target.name);
+        this.setState({ [e.target.name]: e.target.value });
+        console.log(this.state);
+    }
+
     handleSubmit = () => {
         console.log('open a modal');
     }
@@ -98,10 +110,21 @@ class Organization extends React.Component {
     addArchive = () => {
         console.log('add to archived');
     }
-    
+    editModal = (row) => {
+        // console.log(row.original)
+        this.setState({
+            modalVisible: !this.state.modalVisible,
+            orgName: row.original.organization_name,
+            orgAddress: row.original.organization_address,
+            orgType: row.original.organization_type,
+            orgTimeZone: row.original.organization_timezone,
+            orgPlan: row.original.organization_plan
+        })
+    }
+
     toggleModal = () => {
         this.setState({
-            modalVisible: !this.state.modalVisible
+            modalVisible: !this.state.modalVisible,
         })
     }
 
@@ -116,49 +139,56 @@ class Organization extends React.Component {
         return (
             <>
                 <div className="content">
-                    <Modal backdrop={false} isOpen={modalVisible} toggle={this.toggleModal} style={{ width: '100%'}} className='add-project-modal'>
-                        <form onSubmit={this.handleSubmit()}>
-                            <ModalHeader toggle={this.toggleModal}>Add Organization</ModalHeader>
-                            <ModalBody>
-                                <Form>
-                                    <FormGroup row>
-                                        <Label for="name" sm={4}>Organization Name</Label>
-                                        <Col sm={8}>
-                                            <Input type="text"  style={{ marginTop: '0px' }} id="organizationname" placeholder="Organization Name" />
-                                        </Col>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                        <Label for="address" sm={4}>Address</Label>
-                                        <Col sm={8}>
-                                            <Input type="text" style={{ marginTop: '0px' }}  id="address" placeholder="Address" />
-                                        </Col>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                        <Label for="organizationType" sm={4}>Organization Type</Label>
-                                        <Col sm={8}>
-                                            <Input type="select" style={{ marginTop: '0px' }} name="select" id="organizationType" />
-                                        </Col>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                        <Label for="timezone" sm={4}>Time Zone</Label>
-                                        <Col sm={8}>
-                                            <Input type="select" style={{ marginTop: '0px' }} name="select" id="timezone" />
-                                        </Col>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                        <Label for="type" sm={4}>Type</Label>
-                                        <Col sm={8}>
-                                            <Input type="select" style={{ marginTop: '0px' }} name="select" id="type" />
-                                        </Col>
-                                    </FormGroup>
-                                    <FormGroup submit>
-                                        <Col sm={{ size: 10, offset: 2 }}>
-                                            <Button>Submit</Button>
-                                        </Col>
-                                    </FormGroup>
-                                </Form>
-                            </ModalBody>
-                        </form>
+                    <Modal backdrop={false} isOpen={modalVisible} toggle={this.toggleModal} style={{ width: '100%' }} className='add-project-modal'>
+                        <ModalHeader toggle={this.toggleModal}>Add Organization</ModalHeader>
+                        <ModalBody>
+                            <Form onSubmit={this.handleSubmit}>
+                                <FormGroup row>
+                                    <Label for="name" sm={4}>Organization Name</Label>
+                                    <Col sm={8}>
+                                        <Input type="text" value={this.state.orgName} onChange={this.handleChange} style={{ marginTop: '0px' }} name="orgName" id="organizationname" placeholder="Organization Name" />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label for="address" sm={4}>Address</Label>
+                                    <Col sm={8}>
+                                        <Input value={this.state.orgAddress} type="text" onChange={this.handleChange} style={{ marginTop: '0px' }} name="orgAddress" id="address" placeholder="Address" />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label for="organizationType" sm={4}>Organization Type</Label>
+                                    <Col sm={8}>
+                                        <Input value={this.state.orgType} type="select" onChange={this.handleChange} style={{ marginTop: '0px' }} name="orgType" id="organizationType">
+                                            <option>Software Industry</option>
+                                            <option>Construction</option>
+                                            <option>Healthcare</option>
+                                        </Input>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label for="timezone" sm={4}>Time Zone</Label>
+                                    <Col sm={8}>
+                                        <Input value={this.state.orgTimeZone} type="select" onChange={this.handleChange} style={{ marginTop: '0px' }} name="orgTimeZone" id="timezone" />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                    <Label for="plan" sm={4}>Plan</Label>
+                                    <Col sm={8}>
+                                        <Input type="select" onChange={this.handleChange} value={this.state.orgPlan} style={{ marginTop: '0px' }} name="orgPlan" id="type">
+                                            <option>Monitored</option>
+                                            <option>Unmonitored</option>
+                                            <option>Unmonitored Field Service</option>
+                                        </Input>
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup submit>
+                                    <Col sm={{ size: 10, offset: 2 }}>
+                                        <Button>Submit</Button>
+                                    </Col>
+                                </FormGroup>
+                            </Form>
+                        </ModalBody>
+
                     </Modal>
                     <Container>
                         <Row className="tabs-container">
@@ -290,7 +320,7 @@ class Organization extends React.Component {
                                                 Cell: row => (
                                                     <div className='text-center'>
                                                         <span>
-                                                            <i className='fa fa-edit editProject' onClick={() => this.toggleModal('Update project', row.original)} />
+                                                            <i className='fa fa-edit editProject' onClick={() => this.editModal(row)} />
                                                             <Button outline color="primary" size="sm" onClick={this.addArchive}>Archive</Button>
                                                         </span>
                                                     </div>
